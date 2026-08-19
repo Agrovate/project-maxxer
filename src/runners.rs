4829bbd21c492188d0df13982872110ecc58f90e
agrovate <nishantullal@icloud.com>
2026-08-19 10:28:35 +0530

    comments done

--- src/runners.rs
+++ src/runners.rs
@@ -20,45 +20,55 @@ struct Cargo;
 struct Uv;
 struct Make;
 
-
+// Selecting with the help of root makers
 pub fn select_runner(dir: &str) -> Option<Box<dyn Runner>> {
     if Path::new(dir).join("Cargo.toml").exists() {
-        return Some(Box::new(Cargo));
+        Some(Box::new(Cargo))
     }
     else if Path::new(dir).join("pyproject.toml").exists() {
-        return Some(Box::new(Uv));
+        Some(Box::new(Uv))
     }
     else if Path::new(dir).join("Makefile").exists() {
-        return Some(Box::new(Make));
+        Some(Box::new(Make))
     }
-    None
+    else {
+        None
+    }
 }
 
 
+// return arguments to run command: cargo run
 impl Runner for Cargo {
     fn command(&self, _dir:&str) -> Vec<String> {
-        vec!["cargo".to_string(),"run".to_string()]
+        vec!["cargo".into(),"run".into()]
     }
 }
 
-
+// return arguments to run command: uv run <script>
 impl Runner for Uv {
     fn command(&self, dir:&str) -> Vec<String> {
         if let Some(script) = console_script(dir) {
-            return vec!["uv".to_string(), "run".to_string(), script];
+            return vec!["uv".into(), "run".into(), script];
         }
 
         let files = get_files(dir, "py");
+
+        // find the main.py file or the first python file it gets and run that
         let file = files
             .iter()
             .find(|f| Path::new(f).file_name().and_then(|n| n.to_str()) == Some("main.py"))
             .or_else(|| files.first())
             .cloned()
             .unwrap_or_else(|| "main.py".to_string());
-        vec!["uv".to_string(),"run".to_string(), file]
+        vec!["uv".into(),"run".into(), file]
     }
 }
 
+// get path to the pyproject.toml file,
+// get the content present in the pyproject.toml
+// split content to project and scripts
+// if there is only one script then return the value
+// else read the project name and check if name is present in project and if true return name
 fn console_script(dir: &str) -> Option<String> {
     let contents = read_to_string(Path::new(dir).join("pyproject.toml")).ok()?;
     let parsed: PyProject = toml::from_str(&contents).ok()?;
@@ -75,20 +85,32 @@ fn console_script(dir: &str) -> Option<String> {
         .and_then(|name| scripts.contains_key(name).then(|| name.clone()))
 }
 
+
+// return arguments to run command: make
 impl Runner for Make {
     fn command(&self, _dir:&str) -> Vec<String> {
-        vec!["make".to_string()]
+        vec!["make".into()]
     }
 }
 
 
-fn get_files(path:&str, ext:&str) -> Vec<String>{
+fn get_files(path:&str, ext:&str) -> Vec<String> {
+
+    // create a vector of files
     let mut files:Vec<String> = Vec::new();
+
+    // for every path in the directory:
+    //      check if it's a file and if yes check if it has a valid extention
     for entry in read_dir(path).unwrap() {
         let path = entry.unwrap().path();
         if path.is_file() && path.extension().and_then(|e| e.to_str()) == Some(ext) {
-            files.push(path.to_string_lossy().to_string());
+            files.push(path.to_string_lossy().into());
         }
+
+        // check if it is a directory:
+        //      if yes check if it's hidden:
+        //          if yes: ignore
+        //          else: add the files into the main files Vector
         else if path.is_dir() {
             let is_hidden = path
                 .file_name()
